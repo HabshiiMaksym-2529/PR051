45ab4c7a842af2c4dcadb9178c504043826d17b2
HabshiiMaksym-2529 <habshii.maksym.kn@dspu.edu.ua>
2025-11-30 20:15:43 +0200

    PR051TEST

---
+++ UnitTest5.1/UnitTest5.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr5.1/Pr5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double s = 2000;
+			double t = 2;
+			double expected = 0.25;
+			double result = h(t, sqrt(s * s + t * t));
+			Assert::AreEqual(0.25, result);
+		}
+	};
+}
